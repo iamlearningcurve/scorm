1e6c925c778c136676279b224d07ee0db746b230
eshwar <adzenze.eshwar@gmail.com>
2024-02-18 17:36:20 -0500

    making another routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,10 +17,11 @@ import { AppLayoutComponent } from "./layout/app.layout.component";
             //         { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
             //     ]
             // },
+            { path: '', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule)},
             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: '/landing' },
+            { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
     exports: [RouterModule]
